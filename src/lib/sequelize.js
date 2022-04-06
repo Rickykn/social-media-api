5c8e989180195698af39952166c92ea32bd51f7b
Ricky Kurniawan <neakre@Rickys-MacBook-Pro.local>
2022-04-06 14:37:47 +0700

    verification v2

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -11,30 +11,42 @@ const sequelize = new Sequelize({
   //   console.log("SQL AKUUU", sql)
   //   fs.appendFileSync(__dirname + "/sql_logs.txt", sql)
   // }
-  logging: false
-})
+  logging: false,
+});
 
 // Models
 const Post = require("../models/post")(sequelize);
 const User = require("../models/user")(sequelize);
 const Like = require("../models/like")(sequelize);
+const VerificationToken = require("../models/verification_token")(sequelize);
 
 // Associations
 // 1 : M
-Post.belongsTo(User, { foreignKey: "user_id", as: "user_post" })
-User.hasMany(Post, { foreignKey: "user_id", as: "user_post" })
+Post.belongsTo(User, { foreignKey: "user_id", as: "user_post" });
+User.hasMany(Post, { foreignKey: "user_id", as: "user_post" });
 
 // M : M
-Post.belongsToMany(User, { through: Like, foreignKey: "post_id", as: "user_likes" })
-User.belongsToMany(Post, { through: Like, foreignKey: "user_id", as: "user_likes" })
-User.hasMany(Like, { foreignKey: "user_id" })
-Like.belongsTo(User, { foreignKey: "user_id" })
-Post.hasMany(Like, { foreignKey: "post_id" })
-Like.belongsTo(Post, { foreignKey: "post_id" })
+Post.belongsToMany(User, {
+  through: Like,
+  foreignKey: "post_id",
+  as: "user_likes",
+});
+User.belongsToMany(Post, {
+  through: Like,
+  foreignKey: "user_id",
+  as: "user_likes",
+});
+User.hasMany(Like, { foreignKey: "user_id" });
+Like.belongsTo(User, { foreignKey: "user_id" });
+Post.hasMany(Like, { foreignKey: "post_id" });
+Like.belongsTo(Post, { foreignKey: "post_id" });
+
+VerificationToken.belongsTo(User, { foreignKey: "user_id" });
+User.hasMany(VerificationToken, { foreignKey: "user_id" });
 
 module.exports = {
   sequelize,
   Post,
   User,
-  Like
-}
+  Like,
+};
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -10,4 +10,8 @@ router.get("/refresh-token", authorizedLoggedInUser, authControllers.keepLogin);
 router.get("/verify/:token", authControllers.verifyUser);
 router.get("/verify", authorizedLoggedInUser, authControllers.resendVerify);
 
+router.post("/v2/register", authControllers.registerUserV2);
+router.get("/v2/verify/:token", authControllers.verifyUserV2);
+router.get("/verify", authorizedLoggedInUser, authControllers.resendVerifyV2);
+
 module.exports = router;
